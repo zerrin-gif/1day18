9cac52dadf6ed7389ab5e2ffcf1ccc8b43c92dea
Zerrin Ekici <zerkam06@gmail.com>
2020-12-12 18:16:33 -0600

    add first project of day18

---
+++ script.js
@@ -0,0 +1,68 @@
+const jsonButton = document.querySelector('#generate');
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#displayContainer');
+const collection = ["Another", "Oooops", "More", "Next", "Continue", "Keep going", "Click me", "A new one", "Try Again", "So Close", "Well Done"];
+
+const getJson = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+  renderResponse(xhr.response);
+  console.log(xhr.response);
+    }
+  }
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  xhr.send();
+}
+
+
+
+const sendJson = () => {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+}).then(response => response.json())
+  .then(json => console.log(json))
+};
+
+ 
+const formatJson = (resJson) => {
+  resJson = JSON.stringify(resJson);
+  //console.log(resJson);
+  let counter = 0;
+  return resJson.split('')
+  .map(char => {
+    switch (char) {
+      case ',':
+        return `,\n${' '.repeat(counter * 2)}`;
+      case '{':
+        counter += 1;
+        return `{\n${' '.repeat(counter * 2)}`;
+      case '}':
+        counter -= 1;
+        return `\n${' '.repeat(counter * 2)}}`;
+      default:
+        return char;
+    }
+  })
+  .join('');
+}
+
+const renderResponse = (jsonResponse) => {
+  const jsonSelection = Math.floor(Math.random() * 10);
+  sendJson();
+  jsonButton.innerHTML = `${collection[jsonSelection]}!`;
+  display.innerHTML = `<pre>${formatJson(jsonResponse[jsonSelection])}</pre>`;
+}
+
+jsonButton.addEventListener('click', getJson);
+
